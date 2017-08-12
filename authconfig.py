e86f9fd5543bc3cd61c44086cab9cf56018c3ba5
MagaFun <MagaFun@users.noreply.github.com>
2017-08-12 17:05:04 +0800

    Update authconfig.py

--- authconfig.py
+++ authconfig.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-TELEGRAM_BOT_TOKEN = '385322231:AAHdU9QC8krGlPXP0wGyFVTP1XUX9sxiyQw' # 在引号中粘贴你的 Telegram 机器人 token
+TELEGRAM_BOT_TOKEN = '' # 在引号中粘贴你的 Telegram 机器人 token
 TWITTER_TOKEN = { # 在引号中粘贴你的 Twitter 应用程序授权码
-    'consumer_key': '3HQ77IwURs0jeazkZev0HSTcB',
-    'consumer_secret': 'WR1L7OEtOlAqf7dyWg5LBwpMFBUiFYSg2VJKUR0sbJ3D70lol7',
-    'access_token_key': '391360956-vYkeiChTibFaEnN2mbg19r4KCzqPzk8GVXXEPq4Z',
-    'access_token_secret': 'A89An287b9WAgxi6Qn7Y9ggP5nSuLnECy3rQsQGAimVyz'}
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token_key': '',
+    'access_token_secret': ''}
 CHAT_ID = '@MagaFunbotfield' # 在引号中粘贴你的 chat_id 或频道名称
